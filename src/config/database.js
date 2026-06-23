7cf49fee8610cc55c3c9b28bcc8263c547a634a8
yaraqxd-netizen <yaraqxd@gmail.com>
2026-06-23 19:19:53 -0300

    corrigir leitura das variaveis de ambiente

--- src/config/database.js
+++ src/config/database.js
@@ -4,8 +4,8 @@ require("dotenv").config();
 const pool = new Pool({
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
-  user: "postgres" || process.env.DB_USER,
-  password: process.env.DB_PASSWORD || "root",
+  user: process.env.DB_USER || "",
+  password: process.env.DB_PASSWORD || "",
   database: process.env.DB_NAME,
   connectionTimeoutMillis: 5000,
 });
